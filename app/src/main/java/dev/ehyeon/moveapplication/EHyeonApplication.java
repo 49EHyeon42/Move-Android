e88da4df2a7babc71c1b35f4b5e1042a15a53085
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-08-19 20:54:34 +0900

    chore: Hilt 의존성 추가

---
+++ app/src/main/java/dev/ehyeon/moveapplication/EHyeonApplication.java
@@ -0,0 +1,10 @@
+package dev.ehyeon.moveapplication;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class EHyeonApplication extends Application {
+
+}
--- app/src/main/java/dev/ehyeon/moveapplication/MainActivity.java
+++ app/src/main/java/dev/ehyeon/moveapplication/MainActivity.java
@@ -3,12 +3,17 @@ package dev.ehyeon.moveapplication;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import dagger.hilt.android.AndroidEntryPoint;
+import dev.ehyeon.moveapplication.databinding.ActivityMainBinding;
+
+@AndroidEntryPoint
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
