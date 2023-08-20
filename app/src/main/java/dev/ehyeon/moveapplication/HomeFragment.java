1a37aed1580d08bcdd67bbe0083cfa74612d9adb
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-08-20 20:38:57 +0900

    feat: home fragment 데이터 바인딩 적용, 구글 맵 적용

--- app/src/main/java/dev/ehyeon/moveapplication/HomeFragment.java
+++ app/src/main/java/dev/ehyeon/moveapplication/HomeFragment.java
@@ -9,11 +9,23 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.maps.SupportMapFragment;
+
+import dev.ehyeon.moveapplication.databinding.FragmentHomeBinding;
+
 public class HomeFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        FragmentHomeBinding fragmentHomeBinding = FragmentHomeBinding.inflate(inflater, container, false);
+
+        SupportMapFragment googleMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.fragmentHome_supportGoogleMapFragment);
+
+        if (googleMapFragment != null) {
+            googleMapFragment.getMapAsync(new GoogleMapImpl());
+        }
+
+        return fragmentHomeBinding.getRoot();
     }
 }
