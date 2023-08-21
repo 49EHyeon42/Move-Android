cbfe328983bcac9574c5f19aab77721795d2b415
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-08-21 18:41:48 +0900

    feat: location repository 추가
    1. 위도, 경도 제공
    2. 위도, 경도 리스트 제공
    3. 총 이동 거리 제공, 단위 m, 소수점 첫째 자리까지 제공
    4. 최고-현재-평균 속력 제공, 단위 km/h, 소수점 셋째 자리까지 제공
    5. 속력 리스트 제공

---
+++ app/src/main/java/dev/ehyeon/moveapplication/data/distance/LocationRepository.java
@@ -0,0 +1,162 @@
+package dev.ehyeon.moveapplication.data.distance;
+
+import android.annotation.SuppressLint;
+import android.location.Location;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LocationRepository {
+
+    private static final long INTERVAL_MILLIS = 1000;
+
+    private final FusedLocationProviderClient fusedLocationProviderClient;
+
+    private final LocationRequest locationRequest;
+    private final LocationCallback locationCallback;
+
+    private final MutableLiveData<LatLng> currentLatLngMutableLiveData;
+
+    private final List<LatLng> latLngList;
+    private final MutableLiveData<List<LatLng>> latLngListMutableLiveData;
+
+    private Location previousLocation;
+    private final MutableLiveData<Float> totalDistanceMutableLiveData;
+
+    private final MutableLiveData<Float> topSpeedMutableLiveData;
+
+    private final MutableLiveData<Float> currentSpeedMutableLiveData;
+
+    private final MutableLiveData<Float> averageSpeedMutableLiveData;
+
+    private final List<Float> speedList;
+    private final MutableLiveData<List<Float>> speedListMutableLiveData;
+
+    public LocationRepository(FusedLocationProviderClient fusedLocationProviderClient) {
+        this.fusedLocationProviderClient = fusedLocationProviderClient;
+
+        locationRequest = new LocationRequest.Builder(INTERVAL_MILLIS).build();
+        locationCallback = new LocationCallback() {
+
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                Location location = locationResult.getLastLocation();
+
+                if (location != null) {
+                    // latitude, longitude
+                    LatLng newLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+                    currentLatLngMutableLiveData.setValue(newLatLng);
+
+                    latLngList.add(newLatLng);
+                    latLngListMutableLiveData.setValue(latLngList);
+
+                    // distance
+                    if (previousLocation != null) {
+                        totalDistanceMutableLiveData.setValue(
+                                totalDistanceMutableLiveData.getValue() == null ? 0f :
+                                        roundToN(1, totalDistanceMutableLiveData.getValue() + previousLocation.distanceTo(location)));
+                    }
+
+                    previousLocation = location;
+
+                    // Speed
+                    float currentSpeed = roundToN(3, meterPerSecondToKilometerPerHour(location.getSpeed()));
+                    float previousMaxSpeed = topSpeedMutableLiveData.getValue() == null ? 0f : topSpeedMutableLiveData.getValue();
+                    float previousAverageSpeed = averageSpeedMutableLiveData.getValue() == null ? 0f : averageSpeedMutableLiveData.getValue();
+
+                    if (currentSpeed > previousMaxSpeed) {
+                        topSpeedMutableLiveData.setValue(currentSpeed);
+                    }
+
+                    currentSpeedMutableLiveData.setValue(currentSpeed);
+
+                    averageSpeedMutableLiveData.setValue(
+                            previousAverageSpeed == 0f ? currentSpeed : roundToN(3, (currentSpeed + previousAverageSpeed) / 2));
+
+                    speedList.add(currentSpeed);
+                    speedListMutableLiveData.setValue(speedList);
+                }
+            }
+        };
+
+        currentLatLngMutableLiveData = new MutableLiveData<>();
+
+        latLngList = new ArrayList<>();
+        latLngListMutableLiveData = new MutableLiveData<>(latLngList);
+
+        totalDistanceMutableLiveData = new MutableLiveData<>();
+
+        currentSpeedMutableLiveData = new MutableLiveData<>();
+
+        topSpeedMutableLiveData = new MutableLiveData<>();
+
+        averageSpeedMutableLiveData = new MutableLiveData<>();
+
+        speedList = new ArrayList<>();
+        speedListMutableLiveData = new MutableLiveData<>(speedList);
+    }
+
+    @SuppressLint("MissingPermission")
+    public void startSensor() {
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
+    }
+
+    public void stopSensor() {
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+        initAll();
+    }
+
+    public void initAll() {
+        latLngList.clear();
+        previousLocation = null;
+        speedList.clear();
+    }
+
+    public LiveData<LatLng> getCurrentLatLngLiveData() {
+        return currentLatLngMutableLiveData;
+    }
+
+    public LiveData<List<LatLng>> getLatLngListLiveData() {
+        return latLngListMutableLiveData;
+    }
+
+    public LiveData<Float> getTotalDistanceLiveData() {
+        return totalDistanceMutableLiveData;
+    }
+
+    public LiveData<Float> getTopSpeedLiveData() {
+        return topSpeedMutableLiveData;
+    }
+
+    public LiveData<Float> getCurrentSpeedLiveData() {
+        return currentSpeedMutableLiveData;
+    }
+
+    public LiveData<Float> getAverageSpeedMutableLiveData() {
+        return averageSpeedMutableLiveData;
+    }
+
+    public LiveData<List<Float>> getSpeedListLiveData() {
+        return speedListMutableLiveData;
+    }
+
+    private float meterPerSecondToKilometerPerHour(float f) {
+        return (float) (f * 3.6);
+    }
+
+    private float roundToN(int n, float f) {
+        float pow = (float) Math.pow(10, n);
+        return Math.round(f * pow) / pow;
+    }
+}
