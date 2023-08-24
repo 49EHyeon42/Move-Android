9fc40ec18f45790c1c0fc8a81a04e07303dcd641
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-08-24 22:28:26 +0900

    refactor: step repository에서 sensor event listener 분리

---
+++ app/src/main/java/dev/ehyeon/moveapplication/data/step/SensorEventListener2Impl.java
@@ -0,0 +1,42 @@
+package dev.ehyeon.moveapplication.data.step;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener2;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+public class SensorEventListener2Impl implements SensorEventListener2 {
+
+    private final MutableLiveData<Integer> stepMutableLiveData;
+
+    public SensorEventListener2Impl() {
+        this.stepMutableLiveData = new MutableLiveData<>(0);
+    }
+
+    @Override
+    public void onFlushCompleted(Sensor sensor) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (stepMutableLiveData.getValue() != null) {
+            stepMutableLiveData.setValue(stepMutableLiveData.getValue() + (int) event.values[0]);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    public void initializeStep() {
+        stepMutableLiveData.setValue(0);
+    }
+
+    public LiveData<Integer> getStepLiveData() {
+        return stepMutableLiveData;
+    }
+}
--- app/src/main/java/dev/ehyeon/moveapplication/data/step/StepRepository.java
+++ app/src/main/java/dev/ehyeon/moveapplication/data/step/StepRepository.java
@@ -4,23 +4,20 @@ import static android.hardware.SensorManager.SENSOR_DELAY_NORMAL;
 
 import android.content.Context;
 import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener2;
 import android.hardware.SensorManager;
 
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 
 import dev.ehyeon.moveapplication.data.ContextRepository;
 
-public class StepRepository implements ContextRepository, SensorEventListener2 {
+public class StepRepository implements ContextRepository {
 
-    private final MutableLiveData<Integer> stepMutableLiveData;
+    private final SensorEventListener2Impl sensorEventListener;
 
     private SensorManager sensorManager;
 
     public StepRepository() {
-        stepMutableLiveData = new MutableLiveData<>(0);
+        sensorEventListener = new SensorEventListener2Impl();
     }
 
     @Override
@@ -38,9 +35,9 @@ public class StepRepository implements ContextRepository, SensorEventListener2 {
             return;
         }
 
-        initializeStep();
+        sensorEventListener.initializeStep();
 
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR), SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(sensorEventListener, sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR), SENSOR_DELAY_NORMAL);
     }
 
     public void stopStepSensor() {
@@ -48,31 +45,10 @@ public class StepRepository implements ContextRepository, SensorEventListener2 {
             return;
         }
 
-        sensorManager.unregisterListener(this);
-    }
-
-    public void initializeStep() {
-        stepMutableLiveData.setValue(0);
+        sensorManager.unregisterListener(sensorEventListener);
     }
 
     public LiveData<Integer> getStepLiveData() {
-        return stepMutableLiveData;
-    }
-
-    @Override
-    public void onFlushCompleted(Sensor sensor) {
-
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        if (stepMutableLiveData.getValue() != null) {
-            stepMutableLiveData.setValue(stepMutableLiveData.getValue() + (int) event.values[0]);
-        }
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
+        return sensorEventListener.getStepLiveData();
     }
 }
