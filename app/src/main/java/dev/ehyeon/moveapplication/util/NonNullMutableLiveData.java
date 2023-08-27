a5d54fd3bd9de1ede793a41ce30c19e9297990c9
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-08-27 21:35:07 +0900

    feat: live data의 null 문제를 해결하기 위해 NonNullMutableLiveData 추가

---
+++ app/src/main/java/dev/ehyeon/moveapplication/util/NonNullMutableLiveData.java
@@ -0,0 +1,29 @@
+package dev.ehyeon.moveapplication.util;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+
+import java.util.Objects;
+
+public class NonNullMutableLiveData<T> extends LiveData<T> {
+
+    public NonNullMutableLiveData(@NonNull T value) {
+        super(value);
+    }
+
+    @NonNull
+    @Override
+    public T getValue() {
+        return Objects.requireNonNull(super.getValue());
+    }
+
+    @Override
+    public void postValue(@NonNull T value) {
+        super.postValue(value);
+    }
+
+    @Override
+    public void setValue(@NonNull T value) {
+        super.setValue(value);
+    }
+}
