1a5929f17abb06622ef2fce45da64622599d9bed
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-09-01 21:01:02 +0900

    feat: 그래프 임시 적용, fix 필수

--- app/src/main/java/dev/ehyeon/moveapplication/ui/StatisticGraphFragment.java
+++ app/src/main/java/dev/ehyeon/moveapplication/ui/StatisticGraphFragment.java
@@ -9,13 +9,69 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import dev.ehyeon.moveapplication.R;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.ValueFormatter;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import javax.inject.Inject;
+
+import dagger.hilt.android.AndroidEntryPoint;
+import dev.ehyeon.moveapplication.data.local.record.HourlyRecord;
+import dev.ehyeon.moveapplication.data.local.record.HourlyRecordDao;
+import dev.ehyeon.moveapplication.databinding.FragmentStatisticViewpager2GraphBinding;
+
+@AndroidEntryPoint
 public class StatisticGraphFragment extends Fragment {
 
+    @Inject
+    protected HourlyRecordDao hourlyRecordDao;
+
+    private FragmentStatisticViewpager2GraphBinding binding;
+
+    // TODO fix
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_statistic_viewpager2_graph, container, false);
+        binding = FragmentStatisticViewpager2GraphBinding.inflate(inflater, container, false);
+
+        hourlyRecordDao.selectAllHourlyRecordLiveData().observe(getViewLifecycleOwner(), hourlyRecordList -> {
+            List<String> dateList = new ArrayList<>();
+            List<Entry> stepList = new ArrayList<>();
+
+            int i = 0;
+
+            for (HourlyRecord hourlyRecord : hourlyRecordList) {
+                Date date = new Date(hourlyRecord.getId());
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yy-MM-dd HH", Locale.KOREA);
+
+                dateList.add(simpleDateFormat.format(date));
+
+                stepList.add(new Entry(i++, hourlyRecord.getStep()));
+            }
+
+            LineDataSet lineDataSet = new LineDataSet(stepList, "step");
+
+            binding.fragmentStatisticGraphLineChart.setData(new LineData(lineDataSet));
+
+            ValueFormatter formatter = new ValueFormatter() {
+                @Override
+                public String getFormattedValue(float value) {
+                    return dateList.get((int) value);
+                }
+            };
+
+            binding.fragmentStatisticGraphLineChart.getXAxis().setValueFormatter(formatter);
+
+            binding.fragmentStatisticGraphLineChart.invalidate();
+        });
+
+        return binding.getRoot();
     }
 }
