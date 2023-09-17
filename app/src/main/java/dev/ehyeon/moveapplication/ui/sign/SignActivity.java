4d7c5e3a35ba9e34c16e722f204b3de0fc9930bd
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-09-17 16:28:20 +0900

    feat: signUpDialog 적용

--- app/src/main/java/dev/ehyeon/moveapplication/ui/sign/SignActivity.java
+++ app/src/main/java/dev/ehyeon/moveapplication/ui/sign/SignActivity.java
@@ -1,6 +1,8 @@
 package dev.ehyeon.moveapplication.ui.sign;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +16,8 @@ public class SignActivity extends AppCompatActivity {
 
     private SignActivityBinding binding;
 
+    private Dialog signUpDialog;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         SplashScreen.installSplashScreen(this);
@@ -21,5 +25,15 @@ public class SignActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         binding = DataBindingUtil.setContentView(this, R.layout.sign_activity);
+
+        configSignUpDialog();
+
+        binding.signActivitySignUpButton.setOnClickListener(ignored -> signUpDialog.show());
+    }
+
+    private void configSignUpDialog() {
+        signUpDialog = new Dialog(this);
+        signUpDialog.setContentView(R.layout.sign_up_dialog);
+        signUpDialog.getWindow().getAttributes().width = WindowManager.LayoutParams.MATCH_PARENT;
     }
 }
