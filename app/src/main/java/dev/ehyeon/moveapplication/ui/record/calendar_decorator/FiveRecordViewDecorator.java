3a7616a502bf58b2958137266cedfd1bcd301122
TaeHyeon Jo <78364654+49EHyeon42@users.noreply.github.com>
2023-10-13 12:36:33 +0900

    fix: 1km 미만이면 스마일 이미지, 3km 미만이면 별 1, 5km 미만 이면 별 2, 5km 이상이면 별 트로피가 그려지도록 변경

---
+++ app/src/main/java/dev/ehyeon/moveapplication/ui/record/calendar_decorator/FiveRecordViewDecorator.java
@@ -0,0 +1,35 @@
+package dev.ehyeon.moveapplication.ui.record.calendar_decorator;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+import androidx.appcompat.content.res.AppCompatResources;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
+
+import java.util.Set;
+
+import dev.ehyeon.moveapplication.R;
+
+public class FiveRecordViewDecorator implements DayViewDecorator {
+
+    private final Drawable drawable;
+    private final Set<CalendarDay> calendarDays;
+
+    public FiveRecordViewDecorator(Context context, Set<CalendarDay> calendarDays) {
+        drawable = AppCompatResources.getDrawable(context, R.drawable.five);
+        this.calendarDays = calendarDays;
+    }
+
+    @Override
+    public boolean shouldDecorate(CalendarDay day) {
+        return calendarDays.contains(day);
+    }
+
+    @Override
+    public void decorate(DayViewFacade view) {
+        view.setSelectionDrawable(drawable);
+    }
+}
